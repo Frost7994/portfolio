39500e5c7710750be2f44c856e2e52ae5b9fce79
Frost7994 <tommybyrne9@gmail.com>
2024-08-09 00:14:44 +0100

    issue(vercel): deployment issue v3

--- src/components/sections/blog/components/featureTemplates.tsx
+++ src/components/sections/blog/components/featureTemplates.tsx
@@ -86,47 +86,47 @@ export const FeatureTemplate = () => {
   );
 };
 
-// export const FeatureTemplateTwo = () => {
-//   return (
-//     <div className="not-prose border rounded-md bg-gray-100 dark:bg-gray-900 p-2">
-//       <div className="bg-background rounded-md py-24 sm:py-32 px-6 lg:px-8">
-//         <div className="mx-auto max-w-2xl lg:text-center">
-//           <h2 className="text-base font-semibold leading-7 text-brand">
-//             Full toolbox
-//           </h2>
-//           <p className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
-//             Everything you need to deploy your app
-//           </p>
-//           <p className="mt-6 text-lg leading-8 text-muted-foreground">
-//             Quis tellus eget adipiscing convallis sit sit eget aliquet quis.
-//             Suspendisse eget egestas a elementum pulvinar et feugiat blandit at.
-//             In mi viverra elit nunc.
-//           </p>
-//         </div>
-//         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-//           <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-//             {features.map((feature) => (
-//               <div key={feature.name} className="relative pl-16">
-//                 <dt className="text-base font-semibold leading-7 text-primary">
-//                   <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-brand">
-//                     <feature.icon
-//                       aria-hidden="true"
-//                       className="h-6 w-6 text-white"
-//                     />
-//                   </div>
-//                   {feature.name}
-//                 </dt>
-//                 <dd className="mt-2 text-base leading-7 text-muted-foreground">
-//                   {feature.description}
-//                 </dd>
-//               </div>
-//             ))}
-//           </dl>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+export const FeatureTemplateTwo = () => {
+  return (
+    <div className="not-prose border rounded-md bg-gray-100 dark:bg-gray-900 p-2">
+      <div className="bg-background rounded-md py-24 sm:py-32 px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:text-center">
+          <h2 className="text-base font-semibold leading-7 text-brand">
+            Full toolbox
+          </h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
+            Everything you need to deploy your app
+          </p>
+          <p className="mt-6 text-lg leading-8 text-muted-foreground">
+            Quis tellus eget adipiscing convallis sit sit eget aliquet quis.
+            Suspendisse eget egestas a elementum pulvinar et feugiat blandit at.
+            In mi viverra elit nunc.
+          </p>
+        </div>
+        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+            {features.map((feature) => (
+              <div key={feature.name} className="relative pl-16">
+                <dt className="text-base font-semibold leading-7 text-primary">
+                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-brand">
+                    <feature.icon
+                      aria-hidden="true"
+                      className="h-6 w-6 text-white"
+                    />
+                  </div>
+                  {feature.name}
+                </dt>
+                <dd className="mt-2 text-base leading-7 text-muted-foreground">
+                  {feature.description}
+                </dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 // export const FeatureTemplateThree = () => {
 //   return (
@@ -197,8 +197,7 @@ export const ToggleFeatureTemplates = () => {
     {
       id: "feature-2",
       label: "Feature 2",
-      // content: <FeatureTemplateTwo />,
-      content: <FeatureTemplate />,
+      content: <FeatureTemplateTwo />,
     },
     {
       id: "feature-3",
